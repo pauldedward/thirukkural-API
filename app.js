87720444b112a40b7cb09413d637895583cc4334
unknown <edward2000ed@gmail.com>
2021-05-23 19:57:39 +0530

    added from route

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ import _ from "lodash"
 const { Schema } = mongoose;
 
 const app = express()
-mongoose.connect("mongodb+srv://admin-user:"+ process.env.PASSWORD +"@cluster0.hyqg9.mongodb.net/"+ process.env.DBNAME +"?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-const db = mongoose.connection;
+const dbConnect = (async () => {
+  try {
+    
+    await mongoose.connect("mongodb+srv://admin-user:"+ process.env.PASSWORD +"@cluster0.hyqg9.mongodb.net/"+ process.env.DBNAME +"?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log("db connected"));
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
- console.log("db connected");
-});
+    const db = mongoose.connection;
 
+  } catch (err) {
+    console.log('error: ' + err)
+  }
+})();
 
 const itemSchema = new Schema( {
     
@@ -79,21 +83,51 @@ const Item = mongoose.model("Item", itemSchema);
 const Detail = mongoose.model("Detail", detailSchema);
 
 app.get("/", (req, res) => {
-  res.send('Hello World! LOL')
+  res.send("hello guts")
 })
 
-// app.get("/from/:kuralNeeded", (req, res) => {
+app.get("/from/:kuralNeeded", async function (req, res) {
+  
+  const kural = req.params.kuralNeeded;
+  let kuralName =  _.camelCase(kural);
+  kuralName = _.startCase(kural);
 
-// })
+  kuralName = await Detail.findOne({ transliteration : kuralName } , function(err, detail) {
+    if(err) {
+        console.log(err) ;   
+    } else if (!detail){
+        res.send("No such thirukkural topic");
+    } else {
+        return detail;
+    }
+  });
 
-app.get("/:kuralNeeded", (req, res) => {
+  const start = kuralName.start;
+  const end = kuralName.end;
+
+  const kuralArray = await Item.find({ $and : [ {Number : { $gte : start }}, {Number : { $lte : end }} ] } , function (err, items) {
+              if(err) {
+                console.log(err)
+              } else if(!items) {
+                console.log("no items")
+              } else {
+                return items
+              }
+        })
+  kuralArray.sort((a, b) => a.Number - b.Number );
+
+  res.send(kuralArray);
+
+});
+
+app.get("/:kuralNeeded", async function (req, res) {
 
     const kural = req.params.kuralNeeded;
     const kuralNumber = parseInt(kural);
     
     if(kuralNumber) {
 
-        Item.findOne({ Number : kuralNumber } , function(err, item) {
+        await Item.findOne({ Number : kuralNumber } , function(err, item) {
           if(err) {
               console.log(err) ;   
           } else if (!item){
@@ -110,7 +144,7 @@ app.get("/:kuralNeeded", (req, res) => {
           let kuralId = Math.floor(Math.random() * 1330 + 1);
           res.redirect("/" + kuralId);
         } else {
-          Detail.findOne({ transliteration : kuralName } , function(err, detail) {
+          await Detail.findOne({ transliteration : kuralName } , function(err, detail) {
             if(err) {
                 console.log(err) ;   
             } else if (!detail){
@@ -127,6 +161,8 @@ app.get("/:kuralNeeded", (req, res) => {
     }
    
 });
+
+
 
 
 
